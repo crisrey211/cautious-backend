8bba0423cfbe02e35bcc6120bddb91e34013ddc4
christian_reynaltt <creynaltt@maristak.net>
2024-08-17 22:03:12 +0200

    Error: env  variable fixed

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import authRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 
 const app = express()
-app.use(cors({ origin: `${CORS_ORIGIN}`, credentials: true }))
+/* app.use(cors({ origin: 'http://localhost:5173', credentials: true })) */
+app.use(cors({ origin: `${process.env.CORS_ORIGIN}`, credentials: true }))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
